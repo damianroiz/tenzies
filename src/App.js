da5375e5687b386d862073dfc9e2b2b3371f3539
Damian <58891112+damianroiz@users.noreply.github.com>
2022-09-09 16:22:05 -0400

    useEffect()

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ import {nanoid} from "nanoid"
 export default function App() {
   
   const [dice, setDice] = React.useState(allNewDice())
+  const [tenzies, setTenzies] = React.useState(false)
+
+  React.useEffect(() => {
+      console.log("Dice state changed")
+  }, [dice])
+
+
 
   function generateNewDie() {
     return (
